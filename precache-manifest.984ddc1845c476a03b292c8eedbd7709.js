b293e3980dc7684ccef31fec7b7dc033c1131dba
H4D3ZS <adingferrer@yahoo.com>
2021-02-23 21:49:45 +0800

    Updates

---
+++ precache-manifest.984ddc1845c476a03b292c8eedbd7709.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c790aaf4b9176cd3547db2d23fcd1b11",
+    "url": "/WebsitePortfolio/index.html"
+  },
+  {
+    "revision": "3bbc4748706b90db956e",
+    "url": "/WebsitePortfolio/static/css/main.80a930bb.chunk.css"
+  },
+  {
+    "revision": "f35345d3b6d74c9a9dfe",
+    "url": "/WebsitePortfolio/static/js/2.00b4765e.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/WebsitePortfolio/static/js/2.00b4765e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bbc4748706b90db956e",
+    "url": "/WebsitePortfolio/static/js/main.223f1db4.chunk.js"
+  },
+  {
+    "revision": "d181eb5ce890875b61e8",
+    "url": "/WebsitePortfolio/static/js/runtime-main.887c73ef.js"
+  },
+  {
+    "revision": "f5711eaa4996e1483f221e7c57cb7905",
+    "url": "/WebsitePortfolio/static/media/algo.f5711eaa.svg"
+  },
+  {
+    "revision": "95b0034d3d7a7b52f85cd16cecef1ddf",
+    "url": "/WebsitePortfolio/static/media/alibaba.95b0034d.jpg"
+  },
+  {
+    "revision": "b9d16f58b6b954a0e995792873aab138",
+    "url": "/WebsitePortfolio/static/media/amazon.b9d16f58.jpg"
+  },
+  {
+    "revision": "63f68ccc85cc2ab7da913353c08cccaf",
+    "url": "/WebsitePortfolio/static/media/api.63f68ccc.svg"
+  },
+  {
+    "revision": "a055c1a81bff8bbbb31d7be0f5f9aa36",
+    "url": "/WebsitePortfolio/static/media/arybba.a055c1a8.jpg"
+  },
+  {
+    "revision": "a42040d5c68d4d3fed9b27878d2fcca5",
+    "url": "/WebsitePortfolio/static/media/backend.a42040d5.svg"
+  },
+  {
+    "revision": "253ba6746e03972ac3cab7b50fb2c50b",
+    "url": "/WebsitePortfolio/static/media/bg.253ba674.jpg"
+  },
+  {
+    "revision": "ca042db5d0a17f1f6ee36ea90fa2f3a4",
+    "url": "/WebsitePortfolio/static/media/computer.ca042db5.svg"
+  },
+  {
+    "revision": "0a27bbff6aa4dfb3bf8606c2fba69fd6",
+    "url": "/WebsitePortfolio/static/media/facebook.0a27bbff.svg"
+  },
+  {
+    "revision": "cbc38ef18c5a6ecdc0ace14b23e84380",
+    "url": "/WebsitePortfolio/static/media/github.cbc38ef1.svg"
+  },
+  {
+    "revision": "5da5692dd776140e39c94085bfba3cf0",
+    "url": "/WebsitePortfolio/static/media/goog.5da5692d.jpg"
+  },
+  {
+    "revision": "d08bce0dcb53ad1ef20b0cae18977145",
+    "url": "/WebsitePortfolio/static/media/instagram.d08bce0d.svg"
+  },
+  {
+    "revision": "1b97d709e37f1663ec4439ae2d5b9f98",
+    "url": "/WebsitePortfolio/static/media/moba.1b97d709.jpg"
+  },
+  {
+    "revision": "64c56c72135ba0b4ddf8da186182b723",
+    "url": "/WebsitePortfolio/static/media/pin.64c56c72.svg"
+  },
+  {
+    "revision": "1281f56490f8cf68a673de9c4e3c0ece",
+    "url": "/WebsitePortfolio/static/media/port.1281f564.jpg"
+  },
+  {
+    "revision": "3a196d849ac346c1cd38293ab1199c03",
+    "url": "/WebsitePortfolio/static/media/pro.3a196d84.jpg"
+  },
+  {
+    "revision": "9be9ffe82154cffd53f6bc6a4ab6a8b7",
+    "url": "/WebsitePortfolio/static/media/prof.9be9ffe8.jpg"
+  },
+  {
+    "revision": "b5fac2be7a6a8edc808f550a24217391",
+    "url": "/WebsitePortfolio/static/media/puzzle.b5fac2be.svg"
+  },
+  {
+    "revision": "26c488dd1be55304e547e30598fb5ca8",
+    "url": "/WebsitePortfolio/static/media/python.26c488dd.svg"
+  },
+  {
+    "revision": "2e01c74fe6fcd933e0e9f338e56c0922",
+    "url": "/WebsitePortfolio/static/media/react.2e01c74f.svg"
+  },
+  {
+    "revision": "1464053c658e62318c689e56c4f1643c",
+    "url": "/WebsitePortfolio/static/media/repair.1464053c.svg"
+  },
+  {
+    "revision": "2942bfabb3d05332b66eb128e0842cff",
+    "url": "/WebsitePortfolio/static/media/resume.2942bfab.pdf"
+  },
+  {
+    "revision": "a9a99781116126355fb66e12c15f4e35",
+    "url": "/WebsitePortfolio/static/media/snap.a9a99781.jpg"
+  },
+  {
+    "revision": "510e66ea32224bb50f0ecf539d8691b8",
+    "url": "/WebsitePortfolio/static/media/telstra.510e66ea.jpg"
+  },
+  {
+    "revision": "4a04f9f0dd3f59df96497cc7c3021550",
+    "url": "/WebsitePortfolio/static/media/tie.4a04f9f0.svg"
+  },
+  {
+    "revision": "bdf7c4b860fddb06f4b2248d15c586fe",
+    "url": "/WebsitePortfolio/static/media/zoom.bdf7c4b8.jpg"
+  }
+]);
